a8d65f6da6aa6e4525fd2cc408b732c11a40bd42
Yasuhiro ISHII <ishii.yasuhiro@gmail.com>
2012-10-14 00:26:52 +0900

    replaced with the peripheral initialization for STM32F4

--- ssp/target/stm32f4_discovery/target_config.c
+++ ssp/target/stm32f4_discovery/target_config.c
@@ -6,7 +6,10 @@
  *                              Toyohashi Univ. of Technology, JAPAN
  *  Copyright (C) 2005-2007 by Embedded and Real-Time Systems Laboratory
  *              Graduate School of Information Science, Nagoya Univ., JAPAN
- * 
+ *
+ *  Ported to STM32F4(-discovery) by Yasuhiro ISHII 2012
+ *  Email : ishii.yasuhiro@gmail.com
+ *
  *  上記著作権者は，以下の(1)〜(4)の条件を満たす場合に限り，本ソフトウェ
  *  ア（本ソフトウェアを改変したものを含む．以下同じ）を使用・複製・改
  *  変・再配布（以下，利用と呼ぶ）することを無償で許諾する．
@@ -48,38 +51,59 @@
 #include "target_syssvc.h"
 
 
-/*
- * GPIOレジスタ操作関数
+
+/* GPIO port mode register
+   00b : Input
+   01b : General purpose output mode
+   10b : Alternate function mode
+   11b : Analog mode
+*/
+Inline void set_port_mode(uint32_t reg,uint_t p,int_t v)
+{
+  sil_andw((void*)GPIO_MODER(reg),~(uint32_t)((0x03L << (p << 1))));
+  sil_orw((void*)GPIO_MODER(reg),((uint32_t)(v & 0x03) << (p << 1)));
+}
+
+/* GPIO port output speed register
+   00b :   2MHz
+   01b :  25MHz
+   10b :  50MHz
+   11b : 100MHz
+*/
+Inline void set_port_speed(uint32_t reg,uint_t p,int_t v)
+{
+  sil_andw((void*)GPIO_OSPEEDR(reg),~(0x03L << (p << 1)));
+  sil_orw((void*)GPIO_OSPEEDR(reg),((uint32_t)(v & 0x03) << (p << 1)));
+}
+
+/* GPIO port input data register
  */
-Inline void set_cr_mode(uint32_t reg, uint_t p, int_t v)
+Inline int_t get_port_data(uint32_t reg,uint_t p)
 {
-	if (p < 8) {
-		sil_andw((void*)GPIO_CRL(reg), ~CR_MODE_MASK(p));
-		sil_orw((void*)GPIO_CRL(reg), CR_MODE(p, v));
-	} else if (8 <= p && p < 16) {
-		sil_andw((void*)GPIO_CRH(reg), ~CR_MODE_MASK(p - 8));
-		sil_orw((void*)GPIO_CRH(reg), CR_MODE(p - 8, v));
-	}
+  return(!!(sil_rew_mem((void*)GPIO_IDR(reg)) & (1 << p)));
 }
 
-Inline void set_cr_cnf(uint32_t reg, uint_t p, int_t v)
+/* GPIO port output data register
+*/
+Inline void set_port_data(uint32_t reg,uint_t p,int_t v)
 {
-	if (p < 8) {
-		sil_andw((void*)GPIO_CRL(reg), ~CR_CNF_MASK(p));
-		sil_orw((void*)GPIO_CRL(reg), CR_CNF(p, v));
-	} else if (8 <= p && p < 16) {
-		sil_andw((void*)GPIO_CRH(reg), ~CR_CNF_MASK(p - 8));
-		sil_orw((void*)GPIO_CRH(reg), CR_CNF(p - 8, v));
-	}
+  sil_andw((void*)GPIO_ODR(reg),~(1L << p));
+  sil_orw((void*)GPIO_ODR(reg),(v & 0x01) << p);
 }
 
-Inline void set_port_pull(uint32_t reg, uint_t p, bool_t up)
+
+/* GPIO alternate function
+ */
+Inline void select_alternate_port_function(uint32_t reg,uint_t p,int_t v)
 {
-	if (up) {
-		sil_wrw_mem((void*)GPIO_BSRR(reg), 0x01 << p);
-	} else {
-		sil_wrw_mem((void*)GPIO_BRR(reg), 0x01 << p);
-	}
+  if (p < 8){
+    sil_andw((void*)GPIO_AFRL(reg),~(0x0FL << (p << 2)));
+    sil_orw((void*)GPIO_AFRL(reg),((uint32_t)(v & 0x0f) << (p << 2)));
+  } else {
+    p -= 8;
+    sil_andw((void*)GPIO_AFRH(reg),~(0x0FL << (p << 2)));
+    sil_orw((void*)GPIO_AFRH(reg),((uint32_t)(v & 0x0f) << (p << 2)));
+  }
 }
 
 /*
@@ -87,88 +111,139 @@ Inline void set_port_pull(uint32_t reg, uint_t p, bool_t up)
  */
 void target_initialize(void)
 {
-	/*
-	 *  プロセッサクロック(RCC)の初期化
-	 */
-	/* HSEの有効化 */
-	sil_orw((void*)RCC_CR, CR_HSE_ON);
 
-	/* HSE有効待ち */
-	while ((sil_rew_mem((void*)RCC_CR) & CR_HSE_RDY) == 0)
-		;
+  sil_orw((void*)RCC_CR, CR_HSI_ON);
+  
+  // Reset CFGR Register : yishii
+  sil_wrw_mem((void*)RCC_CFGR,0L);
+  
+  // Reset HSEON,CSSON and PLLON bits : yishii
+  sil_andw((void*)RCC_CR,0xfef6ffff);
+  
+  sil_wrw_mem((void*)RCC_PLLCFGR,0x24003010);
+  
+  // reset HSEBYP bit
+  sil_andw((void*)RCC_CR,0xfffbffff);
+  
+  sil_wrw_mem((void*)RCC_CIR,0L);
+  
+  ///////////////////////////////////////////////////////////////
+  // SetSysClock
+  
+  // Enable HSE
+  sil_orw((void*)RCC_CR, CR_HSE_ON);
+  
+  while ((sil_rew_mem((void*)RCC_CR) & CR_HSE_RDY) == 0);
 
-	/* FLASH ROMは2wait待ち */
-	sil_andw((void*)FLASH_ACR, ~ACR_LATENCY_MASK);
-	sil_orw((void*)FLASH_ACR, ACR_LATENCY_TWO);
+  sil_orw((void*)RCC_APB1ENR,0x10000000); // APB1ENR.PWREN = set
+  sil_orw((void*)PWR_CR,PWR_CR_VOS);
 
-	/* HCLK = SYSCLK, PCLK2 = HCLK, PCLK1 = HCLK/2  */
-	sil_orw((void*)RCC_CFGR, 0x00 | (0x00 << 11) | (0x04 << 8))
-		;
+  // HCLK = SYSCLK / 1
+  sil_orw((void*)RCC_CFGR,0);
 
-	/* PLLCLK = 8MHz x 9 = 72MHz, HSE as PLL clock */
-	sil_orw((void*)RCC_CFGR, 0x07 << 18);
-	sil_orw((void*)RCC_CFGR, CFGR_PLL_SRC);
+  // PCLK2 = HCLK / 2
+  sil_orw((void*)RCC_CFGR,0x00008000);
 
-	/* PLLの有効化 */
-	sil_orw((void*)RCC_CR, CR_PLL_ON);
+  // PCLK1 = HCLK / 4
+  sil_orw((void*)RCC_CFGR,0x00001400);
 
-	/* PLL有効待ち */
-	while ((sil_rew_mem((void*)RCC_CR) & CR_PLL_RDY) == 0)
-		;
+  // Configure the main PLL
 
-	/* PLLをシステムクロックに選択 */
-	sil_orw((void*)RCC_CFGR, CFGR_SW_PLL);
+#define PLL_M      8
+#define PLL_N    336
+#define PLL_P      2
+#define RCC_PLLCFGR_PLLSRC_HSE              ((uint32_t)0x00400000)
+#define PLL_Q      7
 
-	/* PLLのシステムクロック選択待ち */
-	while ((sil_rew_mem((void*)RCC_CFGR) & CFGR_SWS_MASK) != (CFGR_SW_PLL << 2))
-		;
+  sil_wrw_mem((void*)RCC_PLLCFGR,
+	      PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) |
+	      (RCC_PLLCFGR_PLLSRC_HSE) | (PLL_Q << 24));
+
+  sil_orw((void*)RCC_CR,CR_PLL_ON);
+
+  // wait for PLL Ready
+  while ((sil_rew_mem((void*)RCC_CR) & CR_PLL_RDY) == 0);
+
+  /* Configure Flash prefetch, Instruction cache, Data cache and wait state */
+  sil_wrw_mem((void*)FLASH_ACR,FLASH_ACR_PRFTEN | FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_LATENCY_5WS);
+
+  // Select the main PLL as system clock source
+  sil_andw((void*)RCC_CFGR,(uint32_t)~CFGR_SW_MASK);
+  sil_orw((void*)RCC_CFGR,CFGR_SW_PLL);
+
+
+	// AHB1 GPIO Clock enable for STM32F4
+	sil_orw((void*)RCC_AHB1ENR,0x000001FF);
+
+	//  all enable(tmp)
+	sil_orw((void*)RCC_APB1ENR,0x36fec9ff);
+
+	// APB2 USART1 Clock enable for STM32F4
+	sil_orw((void*)RCC_APB2ENR,0x00000010);
+
+	// all enable(tmp)
+	sil_orw((void*)RCC_APB2ENR,0x00075f33);
 
 
 	/*
 	 *  プロセッサ依存部の初期化
 	 */
 	prc_initialize();
-
-	/*
-	 *  ペリフェラルの有効化
-	 */
-	sil_orw((void*)RCC_APB2ENR, APB2ENR_USART1_EN | APB2ENR_IOPA_EN |
-			APB2ENR_IOPC_EN | APB2ENR_AFIO_EN);
-#if (TNUM_PORT >= 2)
-	sil_orw((void*)RCC_APB2ENR, APB2ENR_IOPD_EN);
-	sil_orw((void*)RCC_APB1ENR, APB1ENR_USART2_EN);
-#endif
 	/*
 	 *  I/Oポートの初期化
 	 */
-	/* USART1(RX)  プルアップ */
-	set_cr_mode(GPIOA_BASE, 10, MODE_INPUT);
-	set_cr_cnf(GPIOA_BASE, 10, CNF_IN_FLOATING);
+
+
+
+	/* USART1 @ USART1_REMAP = 0
+	   TX : PA9
+	   RX : PA10
+	*/
+
+	/* USART1(RX) */
+	//set_port_mode(GPIOA_BASE,10,2);
+	set_port_mode(GPIOB_BASE,7,2); // test!!
+
 
 	/* USART1(TX) */
-	set_cr_mode(GPIOA_BASE, 9, MODE_OUTPUT_50MHZ);
-	set_cr_cnf(GPIOA_BASE, 9, CNF_OUT_AF_PP);
+	//set_port_speed(GPIOA_BASE,9,MODE_OUTPUT_50MHZ);
+	//set_port_mode(GPIOA_BASE,9,2);
 
-#if (TNUM_PORT >= 2)
-	/* USART2(RX)  プルアップ */
-	set_cr_mode(GPIOD_BASE, 6, MODE_INPUT);
-	set_cr_cnf(GPIOD_BASE, 6, CNF_IN_FLOATING);
+	set_port_speed(GPIOB_BASE,6,MODE_OUTPUT_50MHZ);
+	set_port_mode(GPIOB_BASE,6,2);
 
-	/* USART2(TX) */
-	set_cr_mode(GPIOD_BASE, 5, MODE_OUTPUT_50MHZ);
-	set_cr_cnf(GPIOD_BASE, 5, CNF_OUT_AF_PP);
-
-	/* USART2ポートのリマップ（PD5,6） */
-	sil_orw((void*)AFIO_MAPR, MAPR_USART2_REMAP);
-#endif
+	/* select alternate pin function */
+	//select_alternate_port_function(GPIOA_BASE,9,7);
+	//select_alternate_port_function(GPIOA_BASE,10,7);
+	select_alternate_port_function(GPIOB_BASE,7,7);
+	select_alternate_port_function(GPIOB_BASE,6,7);
 	/* LEDポート */
-	set_cr_mode(GPIOC_BASE, 6, MODE_OUTPUT_50MHZ);
-	set_cr_cnf(GPIOC_BASE, 6, CNF_OUT_GP_PP);
+
+	// for STM32F4-discovery
+
+	set_port_speed(GPIOD_BASE,12,MODE_OUTPUT_50MHZ);
+	set_port_speed(GPIOD_BASE,13,MODE_OUTPUT_50MHZ);
+	set_port_speed(GPIOD_BASE,14,MODE_OUTPUT_50MHZ);
+	set_port_speed(GPIOD_BASE,15,MODE_OUTPUT_50MHZ);
+
+	// LED(ORANGE)
+	set_port_mode(GPIOD_BASE,13,1);
+	set_port_data(GPIOD_BASE,13,1); // for debug
+	// LED(GREEN)
+	set_port_mode(GPIOD_BASE,12,1);
+	set_port_data(GPIOD_BASE,12,1); // for debug
+	// LED(RED)
+	set_port_mode(GPIOD_BASE,14,1);
+	set_port_data(GPIOD_BASE,14,1); // for debug
+	// LED(BLUE)
+	set_port_mode(GPIOD_BASE,15,1);
+	set_port_data(GPIOD_BASE,15,1); // for debug
 
 	/*
 	 *  バナー出力用のシリアル初期化
 	 */
 	target_low_output_init(SIO_PORTID);
+
 }
 
 /*
@@ -193,5 +268,3 @@ void target_fput_log(char_t c)
 	}
 	sio_pol_snd_chr(c, SIO_PORTID);
 }
-
-
